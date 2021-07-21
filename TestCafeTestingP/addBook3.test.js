d84d27f7336c75d413af750f7a7b7dda792c3da0
PrageethThilina <prageeththilina8@gmail.com>
2021-07-21 22:11:56 +0530

    UI Redesign Completed

--- TestCafeTestingP/addBook3.test.js
+++ TestCafeTestingP/addBook3.test.js
@@ -1,4 +1,4 @@
-//testcafe chrome ./TestCafeTestingP/addBook2.test.js
+//testcafe chrome ./TestCafeTestingP/addBook3.test.js
 
 import { Selector, Role } from 'testcafe';
 
@@ -14,19 +14,19 @@ const admin = Role('http://localhost:3000/auth/admin-login', async t => {
 fixture`My fixture`
     .page`http://localhost:3000/auth/admin-login`;
 
-test('Admin Add a Book Test With empty book title', async t => {
+test('Admin Add a Book Test', async t => {
         await t
     
-            .setTestSpeed(0.25)
+            .setTestSpeed(0.2)
             .typeText('#username', 'prageeth')
             .typeText('#password', '111111')
             .click('#btn_submit')
             .click("#add_books")
-            //.typeText('#book-title', 'Madoldoowa')
-            .typeText('#book-author', 'Martin Wickramasingha')
-            .typeText('#book-ISBN', '978-1-4088-5567-9')
+            .typeText('#book-title', 'Sherlock Holmes')
+            .typeText('#book-author', 'Chandana Mendis')
+            .typeText('#book-ISBN', '978-1-4088-5567-7')
             .typeText('#book-category', 'Fiction')
-            .typeText('#book-stock', '10')
+            .typeText('#book-stock', '5')
             .typeText('#book-description', 'Famous Fiction')
             .click('#add-book-btn');
     
